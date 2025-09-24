fb317c7b6893bc0dfaa36dce8e87de3a29fc375f
Sunil Poudel <sunil.poudel@miu.edu>
2025-09-23 20:44:07 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Next.js Admin 
+
+### Run locally
+
+1. **Clone the repository**
+   ```bash
+   git clone https://github.com/arhamkhnz/next-shadcn-admin-dashboard.git
+   ```
+   
+2. **Install dependencies**
+   ```bash
+    npm install
+   ```
+
+3. **Start the development server**
+   ```bash
+   npm run dev
+   ```
+
+Once running, the app will be available at [http://localhost:3000](http://localhost:3000)
+
+---
---
+++ src/navigation/sidebar/sidebar-items.ts
@@ -0,0 +1,85 @@
+import {
+  ShoppingBag,
+  Forklift,
+  Mail,
+  MessageSquare,
+  Calendar,
+  Kanban,
+  ReceiptText,
+  Users,
+  Lock,
+  Fingerprint,
+  SquareArrowUpRight,
+  LayoutDashboard,
+  ChartBar,
+  Banknote,
+  Gauge,
+  GraduationCap,
+  type LucideIcon,
+} from "lucide-react";
+
+export interface NavSubItem {
+  title: string;
+  url: string;
+  icon?: LucideIcon;
+  comingSoon?: boolean;
+  newTab?: boolean;
+  isNew?: boolean;
+}
+
+export interface NavMainItem {
+  title: string;
+  url: string;
+  icon?: LucideIcon;
+  subItems?: NavSubItem[];
+  comingSoon?: boolean;
+  newTab?: boolean;
+  isNew?: boolean;
+}
+
+export interface NavGroup {
+  id: number;
+  label?: string;
+  items: NavMainItem[];
+}
+
+export const sidebarItems: NavGroup[] = [
+  {
+    id: 1,
+    label: "Dashboards",
+    items: [
+      {
+        title: "Default",
+        url: "/dashboard/default",
+        icon: LayoutDashboard,
+      },
+      {
+        title: "Payment",
+        url: "/dashboard/payment",
+        icon: Banknote,
+      },
+      {
+        title: "Report",
+        url: "/dashboard/report",
+        icon: ChartBar,
+      },
+    ],
+  },
+  {
+    id: 2,
+    label: "Pages",
+    items: [
+      {
+        title: "Authentication",
+        url: "/auth",
+        icon: Fingerprint,
+        subItems: [
+          { title: "Login v1", url: "/auth/v1/login", newTab: true },
+          { title: "Login v2", url: "/auth/v2/login", newTab: true },
+          { title: "Register v1", url: "/auth/v1/register", newTab: true },
+          { title: "Register v2", url: "/auth/v2/register", newTab: true },
+        ],
+      },
+    ],
+  },
+];
